cb76d45d01955187bb5fef60ba031744b46424c5
Michael Worm <michael.worm@essquare.de>
2021-07-14 15:10:02 +0200

    Add api route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,12 @@ import { Router, Request, Response, NextFunction } from "express"
 const routes = Router()
 
 routes.get("/", (req: Request, res: Response, next: NextFunction): void => {
-  res.send("hello")
+  res.redirect(301, "/api/images")
+  next()
+})
+
+routes.get("/api/images", (req: Request, res: Response, next: NextFunction): void => {
+  res.send(`Welcome!`)
   next()
 })
 
