b9f306c6e8914119240c94da03d50d1e260184bf
Michael Worm <michael.worm@essquare.de>
2021-07-14 17:33:36 +0200

    Remove dist

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = express_1.Router();
-routes.get("/", function (req, res, next) {
-    res.send("hello");
-    next();
-});
-exports.default = routes;
